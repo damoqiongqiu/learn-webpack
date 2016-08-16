e9518523c4119d3a26fc668f2e99e2b5b3fc7b04
zhang.xiaofei10 <zhang.xiaofei10@zte.com.cn>
2016-08-16 11:52:50 +0800

    插件用法；

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
- module.exports = {
-     entry: './app/index.js',
-     output: {
-         path: './dist',
-         filename: 'index.bundle.js'
-     },
-     module: {
-         loaders: [{
-             test: /\.js$/,
-             exclude: /node_modules/,
-             loader: 'babel-loader'
-         }]
-     }
- };
+const webpack = require('webpack');
+module.exports = {
+    entry: './app/index.js',
+    output: {
+        path: './dist',
+        filename: 'index.bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+            },
+            output: {
+                comments: false,
+            }
+        })
+    ]
+};
