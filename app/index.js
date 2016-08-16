c8e28d9f77b488dc7e268890c829a4cd4d52cdca
zhang.xiaofei10 <zhang.xiaofei10@zte.com.cn>
2016-08-16 20:00:13 +0800

    加上CSS和LESS的例子；

--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
+//import 是ES6的模块化语法，我们这里用了babel插件来编译
  import 'babel-polyfill';
  import cats from './cats';
  import $ from 'jquery';
+
+//混用require语法也可以的，因为有babel
+require("./index.css");
+require("./index.less");
 
  $('<h1>Cats</h1>').appendTo('body');
  const ul = $('<ul></ul>').appendTo('body');
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
- module.exports = {
-     entry: './app/index.js',
-     output: {
-         path: './dist',
-         filename: 'index.bundle.js'
-     },
-     module: {
-         loaders: [{
-             test: /\.js$/,
-             exclude: /node_modules/,
-             loader: 'babel-loader'
-         }]
-     }
- };
+const webpack = require('webpack');
+module.exports = {
+    entry: './app/index.js',
+    output: {
+        path: './dist',
+        filename: 'index.bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }, {
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        }, {
+            test: /\.less$/,
+            loader: "style-loader!css-loader!less-loader"
+        }]
+    }
+};
