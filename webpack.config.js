87b7d726ef5ab95a18addff1a9dc4ed8bdc83141
damoqiongqiu <damoqiongqiu@126.com>
2018-08-31 16:51:20 +0800

    auto-generate-index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
     output: {
-        filename: "index.bundle.js",
+        filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist")
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "自动生成index.html"
+        })
+    ],
     module: {
         rules: [
             {
