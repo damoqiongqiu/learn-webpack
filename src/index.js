49afc4166c1a3b4f75400c52a0dcecf5161473ab
damoqiongqiu <damoqiongqiu@126.com>
2019-07-11 19:18:49 +0800

    更新实例代码

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ function component() {
     element.appendChild(button);
 
     button.onclick = e =>
+        //这个import()函数是在ES6的时候提出来的，但是目前还不是正式的ECMA标准
         import("./print").then(module => {
             var print = module.default;
             window.print();
